4d53946c3f7f717a9df7624f3477b27b60c5f83f
paulgibes <paulgibes@gmail.com>
2023-02-19 16:23:41 -0700

    checks the time and changes the class and color of each section

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,10 +1,50 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// Shorthand for $( document ).ready(function () {})
+// in the html. Shorthand for $(document).ready(function () {})
 $(function () {
-  var today = dayjs();
-  console.log(today);
+  var hourSection = [
+    "#hour-1",
+    "#hour-2",
+    "#hour-3",
+    "#hour-4",
+    "#hour-5",
+    "#hour-6",
+    "#hour-7",
+    "#hour-8",
+    "#hour-9",
+    "#hour-10",
+    "#hour-11",
+    "#hour-12",
+    "#hour-13",
+    "#hour-14",
+    "#hour-15",
+    "#hour-16",
+    "#hour-17",
+    "#hour-18",
+    "#hour-19",
+    "#hour-20",
+    "#hour-21",
+    "#hour-22",
+    "#hour-23",
+    "#hour-24",
+  ];
+  var currentHour = dayjs().hour();
+  // console.log(currentHour);
+
+  for (let i = 0; i < hourSection.length; i++) {
+    var hourSectionNum = hourSection.indexOf("#hour-" + [i]) + 1;
+    console.log(hourSectionNum);
+
+    if (currentHour === hourSectionNum) {
+      $(hourSection[i]).addClass("present");
+    } else if (currentHour > hourSectionNum) {
+      $(hourSection[i]).addClass("past");
+    } else if (currentHour < hourSectionNum) {
+      $(hourSection[i]).addClass("future");
+    } else {
+      console.log("Congratulations! You broke time!!");
+    }
+  }
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -16,8 +56,7 @@ $(function () {
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  // past, present, and future classes?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
